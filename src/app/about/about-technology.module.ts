9cffb35d8896fe1f7bf2563ee4587ea6461e8589
Victor García Expósito <victorvgem@gmail.com>
2020-05-28 14:58:21 +0200

    fix scroll

--- src/app/about/about-technology.module.ts
+++ src/app/about/about-technology.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutTechnologyComponent } from './about-technology.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutTechnologyComponent],
   imports: [
     CommonModule
   ],
+  exports: [AboutTechnologyComponent]
 })
 export class AboutTechnologyModule { }
