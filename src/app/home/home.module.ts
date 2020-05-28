5bce1b6c2ff92e8edf4f25d7bb6ec6bbcd88b072
INTERNETL0RD <xtebanchallenger@gmail.com>
2020-05-28 11:43:20 +0200

    home ready

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, HomeComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
