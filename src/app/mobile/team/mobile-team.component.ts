4e6a3e59e3decb0bb75b96103ffc6a716aaca3ef
Victor García Expósito <victorvgem@gmail.com>
2020-06-18 15:03:01 +0200

    DW-21 Done

--- src/app/mobile/team/mobile-team.component.ts
+++ src/app/mobile/team/mobile-team.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Globals } from 'src/app/globals';
+import { fade2 } from 'src/app/animations';
 
 @Component({
   selector: 'app-mobile-team',
   templateUrl: './mobile-team.component.html',
-  styleUrls: ['./mobile-team.component.sass']
+  styleUrls: ['./mobile-team.component.sass'],
+  animations: [fade2]
 })
 export class MobileTeamComponent implements OnInit {
+
+  public visibleItemState = [true, false, false, false, false];
 
   constructor(private readonly globals: Globals) { }
 
--- src/app/mobile/team/mobile-team.module.ts
+++ src/app/mobile/team/mobile-team.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobileTeamComponent } from './mobile-team.component';
+import { ScrollerModule } from 'src/app/scroller/scroller.module';
 
 
 
 @NgModule({
   declarations: [MobileTeamComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ScrollerModule
   ],
   exports: [MobileTeamComponent]
 })
